39a62c768b9b65659f8f5375de7e920a122341ee
Matthias Lang <langmattis@gmail.com>
2017-03-28 19:13:17 +0200

    Bugfixes

--- resources/private/js/angular/controller/mainController.js
+++ resources/private/js/angular/controller/mainController.js
@@ -1,22 +1,31 @@
-myApp.controller('mainController', function ($scope, $cordovaPreferences) {
+myApp.controller('mainController', function ($scope, $cordovaPreferences, $http) {
     var nineOClock = new Date();
-    nineOClock.setHours(20,47,0,0);
-    document.addEventListener("deviceready", function () {
-        $cordovaPreferences.fetch('general')
-            .success(function (value) {
-                if (value.notifications) {
-                    cordova.plugins.notification.local.schedule({
-                        id: 1,
-                        at: nineOClock,
-                        every: 'day',
-                        title: 'Dein Lieblingsgericht',
-                        text: "Heute gibt es Burger im Roadhouse. Lass es dir schmecken!",
-                        icon: 'res://icon.png',
-                        smallIcon: 'res://notification.png',
-                        led: '4CA49D'
+    nineOClock.setHours(9,0,0,0);
+
+    $http.get(URL + '/menus')
+        .then(function (response) {
+            var todayMenus = response.data;
+            var todayMenu = todayMenus[0].mainCourse.description;
+            document.addEventListener("deviceready", function () {
+                $cordovaPreferences.fetch('general')
+                    .success(function (value) {
+                        if (value.notifications) {
+                            cordova.plugins.notification.local.schedule({
+                                id: 1,
+                                at: nineOClock,
+                                every: 'minute',
+                                title: 'Dein Lieblingsgericht',
+                                text: todayMenu,
+                                icon: 'res://icon.png',
+                                smallIcon: 'res://notification.png',
+                                led: '4CA49D'
+                            });
+                        }
                     });
-                }
-            });
-    }, false);
-    alert('sadf');
+            }, false);
+        })
+
+    $scope.slide = function(href, direction) {
+        slide(href, direction);
+    }
 });
