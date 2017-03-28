cd0b96813b7f2be571571a07be9edd67b19ef2ce
Matthias Lang <Matthias Lang>
2017-03-28 20:42:52 +0200

    Fixed push notification

--- resources/private/js/angular/controller/mainController.js
+++ resources/private/js/angular/controller/mainController.js
@@ -1,31 +1,51 @@
-myApp.controller('mainController', function ($scope, $cordovaPreferences, $http) {
+myApp.controller('mainController', function ($scope, $cordovaPreferences) {
     var nineOClock = new Date();
-    nineOClock.setHours(9,0,0,0);
+    nineOClock.setHours(9, 0, 0, 0);
+    var interests = {};
 
-    $http.get(URL + '/menus')
-        .then(function (response) {
-            var todayMenus = response.data;
-            var todayMenu = todayMenus[0].mainCourse.description;
-            document.addEventListener("deviceready", function () {
-                $cordovaPreferences.fetch('general')
-                    .success(function (value) {
-                        if (value.notifications) {
-                            cordova.plugins.notification.local.schedule({
-                                id: 1,
-                                at: nineOClock,
-                                every: 'minute',
-                                title: 'Dein Lieblingsgericht',
-                                text: todayMenu,
-                                icon: 'res://icon.png',
-                                smallIcon: 'res://notification.png',
-                                led: '4CA49D'
-                            });
-                        }
+    document.addEventListener("deviceready", function () {
+        $cordovaPreferences.fetch('interests').success(function (value) {
+            interests = value;
+        });
+        $cordovaPreferences.fetch('general')
+            .success(function (value) {
+                if (value.notifications) {
+                    cordova.plugins.notification.local.schedule({
+                        id: 1,
+                        at: nineOClock,
+                        //every: 'minute',
+                        title: 'Dein Lieblingsgericht',
+                        text: getFavouriteMeal(),
+                        icon: 'res://icon.png',
+                        smallIcon: 'res://notification.png',
+                        led: '4CA49D'
                     });
-            }, false);
-        })
+                }
+            });
+    }, false);
 
-    $scope.slide = function(href, direction) {
+    function getFavouriteMeal() {
+        var menu;
+        $.ajax({
+            url: URL + '/menus',
+            success: function (response) {
+                var todayMenus = response;
+                if (todayMenus.length) {
+                    for (var i = 0; i < todayMenus.length; i++) {
+                        for (var j = 0; j < interests.length; j++) {
+                            if (todayMenus[i].mainCourse.description.includes(interests[j].name)) {
+                                menu = todayMenus[i].mainCourse.description;
+                            }
+                        }
+                    }
+                }
+            },
+            async: false
+        });
+        return 'Heute gibt es ' + menu;
+    }
+
+    $scope.slide = function (href, direction) {
         slide(href, direction);
     }
 });
